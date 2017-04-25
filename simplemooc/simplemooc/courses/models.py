8295390845b12bce2f3b7ac3ec0e0c46b3e22307
Thiago Rossener <thiago.rossener@gmail.com>
2017-04-25 13:04:47 -0300

    Starting admin

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -28,3 +28,11 @@ class Course(models.Model):
 
 	objects = CourseManager()
 
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Curso'
+		verbose_name_plural = 'Cursos'
+		ordering = ['name']
+
