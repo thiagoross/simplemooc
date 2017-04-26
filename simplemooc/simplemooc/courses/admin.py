5164cd265299fb863f34c5dfd310dcf706ddcc9f
Thiago Rossener <thiago.rossener@gmail.com>
2017-04-25 22:23:39 -0300

    Page course

--- simplemooc/simplemooc/courses/admin.py
+++ simplemooc/simplemooc/courses/admin.py
@@ -6,5 +6,6 @@ class CourseAdmin(admin.ModelAdmin):
 
 	list_display = ['name', 'slug', 'start_date', 'created_at']
 	search_fields = ['name', 'slug']
+	prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Course, CourseAdmin)
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from simplemooc.courses.models import Course
+
+def index(request):
+	courses = Course.objects.all()
+	template_name = 'courses/index.html'
+	context = {
+		'courses': courses
+	}
+	return render(request, template_name, context)
+
+def details(request, pk):
+	course = get_object_or_404(Course, pk=pk)
+	template_name = 'courses/details.html'
+	context = {
+		'course': course
+	}
+	return render(request, template_name, context)
