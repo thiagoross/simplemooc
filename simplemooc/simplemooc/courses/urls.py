80f03040bfe05b7acf96d1b5db606c7759d820f3
Thiago Rossener <thiago.rossener@gmail.com>
2017-04-26 22:45:17 -0300

    Contact form

--- simplemooc/simplemooc/courses/urls.py
+++ simplemooc/simplemooc/courses/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('simplemooc.courses.views',
     url(r'^$', 'index', name='index'),
-    url(r'^(?P<pk>\d+)/$', 'details', name='details'),
+    # url(r'^(?P<pk>\d+)/$', 'details', name='details'),
+    url(r'^(?P<slug>[\w_-]+)/$', 'details', name='details'),
 )
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from simplemooc.courses.models import Course
+from simplemooc.courses.forms import ContactCourse
 
 def index(request):
 	courses = Course.objects.all()
@@ -10,10 +11,17 @@ def index(request):
 	}
 	return render(request, template_name, context)
 
-def details(request, pk):
-	course = get_object_or_404(Course, pk=pk)
+def details(request, slug):
+	course = get_object_or_404(Course, slug=slug)
+	context = {}
+	if request.method == 'POST':
+		form = ContactCourse(request.POST)
+		if form.is_valid():
+			context['is_valid'] = True
+			form = ContactCourse()
+	else:
+		form = ContactCourse()
+	context['form'] = form
+	context['course'] = course
 	template_name = 'courses/details.html'
-	context = {
-		'course': course
-	}
 	return render(request, template_name, context)
