f1ec32570658daf1428ff90080a109e0e9b8a413
Thiago Rossener <thiago.rossener@gmail.com>
2017-04-30 19:57:39 -0300

    Showing courses

--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import (UserCreationForm, PasswordChangeForm,
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.contrib import messages
 
+from simplemooc.courses.models import Enrollment
 from .forms import RegisterForm, EditAccountForm, PasswordResetForm
 from .models import PasswordReset
 
@@ -13,7 +15,8 @@ User = get_user_model()
 @login_required
 def dashboard(request):
 	template_name = 'accounts/dashboard.html'
-	return render(request, template_name)
+	context = {}
+	return render(request, template_name, context)
 
 def register(request):
 	template_name = 'accounts/register.html'
@@ -41,8 +44,10 @@ def edit(request):
 		form = EditAccountForm(request.POST, instance=request.user)
 		if form.is_valid():
 			form.save()
-			form = EditAccountForm(instance=request.user)
-			context['success'] = True
+			messages.success(
+				request, 'Os dados da sua conta foram alterados com sucesso'
+			)
+			return redirect('accounts:dashboard')
 	else:
 		form = EditAccountForm(instance=request.user)
 	context['form'] = form
