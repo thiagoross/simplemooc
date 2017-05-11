5f0f3bdccfccfa6e8d269465a14c26fc8fda7ce2
Thiago Rossener <thiago.rossener@gmail.com>
2017-05-11 10:53:42 -0300

    Forum with correct answers

--- simplemooc/simplemooc/forum/admin.py
+++ simplemooc/simplemooc/forum/admin.py
@@ -7,11 +7,12 @@ class ThreadAdmin(admin.ModelAdmin):
 
     list_display = ['title', 'author', 'created_at', 'updated_at']
     search_fields = ['title', 'author__email', 'body']
+    prepopulated_fields = {'slug': ('title',)}
 
 
 class ReplyAdmin(admin.ModelAdmin):
 
-    list_display = ['thread', 'author', 'created_at', 'updated_at']
+    list_display = ['thread', 'author', 'correct','created_at', 'updated_at']
     search_fields = ['thread__title', 'author__email', 'reply']
 
 
--- simplemooc/simplemooc/forum/models.py
+++ simplemooc/simplemooc/forum/models.py
@@ -6,6 +6,7 @@ from taggit.managers import TaggableManager
 class Thread(models.Model):
 
     title = models.CharField('Título', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100, unique=True)
     body = models.TextField('Mensagem')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, verbose_name='Autor', related_name='threads'
@@ -19,6 +20,10 @@ class Thread(models.Model):
 
     def __str__(self):
         return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('forum:thread', (), {'slug': self.slug})
 
     class Meta:
         verbose_name = 'Tópico'
@@ -46,3 +51,23 @@ class Reply(models.Model):
         verbose_name = 'Resposta'
         verbose_name_plural = 'Respostas'
         ordering = ['-correct', 'created_at']
+
+
+def post_save_reply(created, instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+    if instance.correct:
+        instance.thread.replies.exclude(pk=instance.pk).update(
+            correct=False
+        )
+
+def post_delete_reply(instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+models.signals.post_save.connect(
+    post_save_reply, sender=Reply, dispatch_uid='post_save_reply'
+)
+models.signals.post_delete.connect(
+    post_delete_reply, sender=Reply, dispatch_uid='post_delete_reply'
+)
